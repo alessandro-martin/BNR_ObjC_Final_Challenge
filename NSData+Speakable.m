73601d8f655fc1fe567935974aa26df45b2511f1
alessandro-martin <martin.acg@gmail.com>
2014-07-06 19:08:25 -0430

    Clearer Syntax For Brands Array

--- NSData+Speakable.m
+++ NSData+Speakable.m
@@ -8,31 +8,28 @@
 
 #import "NSData+Speakable.h"
 
-NSArray *brands;
-
-NSString *intToBrand(int n){
-	if (n < 0 || n > [brands count] - 1) {
-		return @"";
-	} else {
-		return brands[n];
-	}
-}
-
-NSUInteger brandToInt(NSString *brand){
-	return [brands indexOfObject:brand];
-}
-
 @implementation NSData (Speakable)
 
-+ (void) initialize
++ (NSArray *) brands
 {
-	if (!brands) {
-		brands = @[@"Camry", @"Nikon", @"Apple", @"Ford", @"Audi", @"Google", @"Nike", @"Amazon",
-				   @"Honda", @"Mazda", @"Buick", @"Fiat", @"Jeep", @"Lexus", @"Volvo", @"Fuji",
-				   @"Sony", @"Delta", @"Focus", @"Puma", @"Samsung", @"Tivo", @"Halo", @"Sting",
-				   @"Shrek", @"Avatar", @"Shell", @"Visa", @"Vogue", @"Twitter", @"Lego", @"Pepsi"];
+	return @[@"Camry", @"Nikon", @"Apple", @"Ford", @"Audi", @"Google", @"Nike", @"Amazon",
+			 @"Honda", @"Mazda", @"Buick", @"Fiat", @"Jeep", @"Lexus", @"Volvo", @"Fuji",
+			 @"Sony", @"Delta", @"Focus", @"Puma", @"Samsung", @"Tivo", @"Halo", @"Sting",
+			 @"Shrek", @"Avatar", @"Shell", @"Visa", @"Vogue", @"Twitter", @"Lego", @"Pepsi"];
+}
 
++ (NSString *) intToBrand:(int) n
+{
+	if (n < 0 || n > [[self brands] count] - 1) {
+		return @"";
+	} else {
+		return [self brands][n];
 	}
+}
+
++ (NSUInteger) brandToInt:(NSString *)brand
+{
+	return [[NSData brands] indexOfObject:brand];
 }
 
 - (NSString *)encodeAsSpeakableString
@@ -41,8 +38,8 @@ NSUInteger brandToInt(NSString *brand){
 	const uint8_t *bytes = [self bytes];
 	for (int i = 0; i < [self length]; i++) {
 		uint8_t number = ((bytes[i] & 0xe0) >> 5) + 2;// Leftmost 3 bits shifted right 5 and added 2
-		uint8_t brand =  (bytes[i] & 0x1f);// Rightmost 5 bits
-		[result appendFormat:@"%d %@", number, intToBrand(brand)];
+		uint8_t brandCode =  (bytes[i] & 0x1f);// Rightmost 5 bits
+		[result appendFormat:@"%d %@", number, [NSData intToBrand:brandCode]];
 		if (i < [self length] - 1) {
 			[result appendString:@" "];
 		}
@@ -61,8 +58,8 @@ NSUInteger brandToInt(NSString *brand){
 		uint8_t digit = [tokens[i] intValue];
 		digit = (digit - 2) << 5;
 		i++; // Processing pairs of values, this may be a little unorthodox though...
-		if (brandToInt(tokens[i]) != NSNotFound) {
-		 	uint8_t brand = brandToInt(tokens[i]);
+		if ( [NSData brandToInt:tokens[i]] != NSNotFound ) {
+		 	uint8_t brand = [NSData brandToInt:tokens[i]];
 			digit += brand;
 			[result appendBytes:&digit length:1];
 		} else {
